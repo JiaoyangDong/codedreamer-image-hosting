9c95d1a33dd7f958725b97f1169d058855de0313
JiaoyangDong <zoradongccc@gmail.com>
2023-08-04 23:21:00 +0800

    add root

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 app.use("/images", express.static(path.join(__dirname, 'images')))
 
 app.listen(port, () => {
